5a61a98d1f31f73191d80e898698908a83f0b933
LoneCoderLab <joe@lonecoderlab.com>
2018-12-27 17:07:20 +0100

    0.0.4 Implement a clean theme - Errors

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -28,9 +28,10 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  final String title;
 
   const MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
